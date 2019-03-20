1d4f887d4e08fd6be6903902cad0a9ca12168282
lfy0317 <18210049914@163.com>
2019-03-20 21:04:50 +0800

    2019.03.20导航制作

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+/*登录注册*/
 function showQrCode(){
 	$('.top-ul-li-a-last').on({
 		'mouseover' : function(){
@@ -9,3 +10,29 @@ function showQrCode(){
 	})
 }
 showQrCode();
+/*导航*/
+function navHover(){
+	$('#navWrap .nav ul li:nth-child(1) a').addClass('active');
+	$('#navWrap .nav ul li a').mouseenter(function(){
+		$(this).addClass('active');
+	});
+	$('#navWrap .nav ul li a').click(function(){
+		$('#navWrap .nav ul li:nth-child(1) a').removeClass('active');
+		$(this).addClass('change').parent().siblings().children('a').removeClass('change')
+	});
+	$('#navWrap .nav ul li a').mouseleave(function(){
+		$(this).removeClass('active')
+	})
+}
+navHover();
+function searchTxtFocus(){
+	$('#navWrap .nav form #searchTxt').on({
+		'focus' : function(){
+			$(this).attr({"value" : ""})
+		},
+		'blur' : function(){
+			$(this).attr({"value" : "搜索你感兴趣的品牌"})
+		}
+	})
+}
+searchTxtFocus();
